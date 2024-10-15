678d003b690791c014d846a0b156d4aafa023ab4
Piotr Stus <pm2024love@gmail.com>
2024-10-15 22:39:59 +0200

    Items table designed & added.

---
+++ ToDo.SqlDatabase/dbo/Tables/Items.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Items]
+(
+	[Id]				INT				IDENTITY (1, 1) NOT NULL,
+	[Title]				NVARCHAR (100)					NOT NULL,
+	[Description]		NVARCHAR (300)					NULL,
+	[DueDate]			DATETIMEOFFSET					NOT NULL,
+	[IsCompleted]		BIT								NOT NULL DEFAULT 0,
+	[CreatedAt]			DATETIMEOFFSET					NOT NULL DEFAULT GETDATE(),
+	UpdatedAt			DATETIME						NULL
+	CONSTRAINT [PK_Items] PRIMARY KEY CLUSTERED ([Id] ASC)
+);
+
+
+
+GO
+CREATE INDEX [IQ_Items_DueDate] ON [dbo].[Items] ([DueDate]);
+
+GO
+CREATE INDEX [IQ_Items_IsCompleted] ON [dbo].[Items] ([IsCompleted]);
